321ba0dbc30b51da09ee3203cb7dbf4bff69c71e
Rick van Hattem <Wolph@wol.ph>
2020-09-22 03:26:54 +0200

    ip is in /sbin

--- scripts/route-up.sh
+++ scripts/route-up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 set -v
 echo "route up"
-gateway=$(ip route | grep default | awk '{print $3}')
-ip route add 10.192.0.0/10 via $gateway dev eth0
+gateway=$(/sbin/ip route | grep default | awk '{print $3}')
+/sbin/ip route add 10.192.0.0/10 via $gateway dev eth0
 /usr/sbin/sockd -D
 date > /up
