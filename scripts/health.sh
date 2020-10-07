52ec7569c4419c5b94022b546f8e58d406d5d7e1
Rick van Hattem <Wolph@wol.ph>
2020-10-07 17:43:15 +0200

    improved get ip script

--- scripts/health.sh
+++ scripts/health.sh
@@ -4,12 +4,12 @@
 IP="$(get_ip)"
 
 if [ "$IP" = "$BASE_IP" ]; then
-  echo 'VPN not working'
+  echo "VPN not working, got ip: $IP"
   exit 1
 elif [ -z "$IP" ]; then
-  echo 'Unable to get IP'
+  echo "Unable to get ip, base ip: $BASE_IP"
   exit 2
 else
-  echo "IP: $IP"
+  echo "ip: $IP, base ip: $BASE_IP"
   exit 0
 fi
