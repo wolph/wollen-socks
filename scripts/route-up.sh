07035c4f37a6cfeeed425233962ce3d4d5f999a1
Rick van Hattem <wolph@wol.ph>
2020-12-21 14:06:52 +0100

    Added get-ip to route up

--- scripts/route-up.sh
+++ scripts/route-up.sh
@@ -5,3 +5,6 @@ gateway=$(/sbin/ip route | grep default | awk '{print $3}')
 /sbin/ip route add 10.192.0.0/10 via $gateway dev eth0
 /usr/sbin/sockd -D
 date > /up
+
+. ./get_ip.sh
+echo "IP: $(get_ip)"
